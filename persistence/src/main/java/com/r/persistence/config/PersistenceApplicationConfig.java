a3920a69eb06453ad462bbd410b19c208a674338
ganeshaaru <rganesh.aaru@gmail.com>
2020-08-30 05:43:50 +0530

    Simple CURD with Java 9 Modules

--- persistence/src/main/java/com/r/persistence/config/PersistenceApplicationConfig.java
+++ persistence/src/main/java/com/r/persistence/config/PersistenceApplicationConfig.java
@@ -9,15 +9,5 @@ import org.springframework.context.annotation.Configuration;
 @EntityScan(basePackages = "com.r.persistence.entity")
 public class PersistenceApplicationConfig {
 
-  /* @Bean
-  public DataSource dataSource() {
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-      dataSource.setUrl("jdbc:mysql://localhost:3306/bookstore");
-      dataSource.setUsername("root");
-      dataSource.setPassword("Titan^2017");
-
-      return dataSource;
-  }*/
 
 }
